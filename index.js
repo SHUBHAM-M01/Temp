6b5d50ecebb33b084c16e0f860d828fa10aef020
Shubham Maurya <shubhammaurya0923@gmail.com>
2024-11-30 17:42:33 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello this my New Practice Folder");
+const newVariable = "String";
+const newInteger = 1000;
+
+
+const newName = () => {
+    console.log(newVariable); 
+}
+
---
+++ script.js
@@ -0,0 +1,1 @@
+const serverVariable = "Server";
